93cb1d988cddea980a5d848c551afcebd7ce79d7
Fahad <205fahadmohammad@gmail.com>
2026-03-24 12:45:50 +0600

    server crash issue fix add

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,10 +1,19 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient;
+  pool: pg.Pool;
+};
 
 function createPrismaClient() {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+  const pool =
+    globalForPrisma.pool ||
+    new pg.Pool({ connectionString: process.env.DATABASE_URL! });
+  if (process.env.NODE_ENV !== "production") globalForPrisma.pool = pool;
+
+  const adapter = new PrismaPg({ pool });
   return new PrismaClient({ adapter });
 }
 
